aa0bba49ed392dc05aec525d2e54224cae01d012
Nazisg <isgnezrin@gmail.com>
2024-06-10 23:52:36 +0400

    updated

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,9 +1,25 @@
+import React from 'react';
 import Title from "@/shared/components/Title";
+import movie from '@/shared/media/imgs/movie.png';
 
-function Projects() {
+const Projects: React.FC = () => {
   return (
     <div>
-      <Title title={"Projects"} />
+      <Title title="Projects" />
+      <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="w-full flex flex-col gap-2 bg-primary-bg border border-[#383838] p-4 rounded-xl">
+          <img src={movie} alt="movie" className="w-full h-full object-cover rounded-xl" />
+          <h2 className="text-[#f6f6f6] text-xl">
+            StreamVibe - <span className="text-[#f6f6f6b6] font-light">Movie page</span>
+          </h2>
+          <div className="flex flex-wrap gap-2">
+            <div className="bg-primary-bg border border-[#383838] p-2 text-[#d6d6d6] text-xs rounded-2xl">React</div>
+            <div className="bg-primary-bg border border-[#383838] p-2 text-[#d6d6d6] text-xs rounded-2xl">TypeScript</div>
+            <div className="bg-primary-bg border border-[#383838] p-2 text-[#d6d6d6] text-xs rounded-2xl">TMDb API</div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
