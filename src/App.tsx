f2a1de9b01fc16209a8763a8e2eea0d23ec1e6f0
Nazisg <isgnezrin@gmail.com>
2024-06-18 23:15:10 +0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,26 @@ import About from "@/pages/About";
 import Resume from "@/pages/Resume";
 import Sidebar from "@/shared/layout/Sidebar";
 import Header from "@/shared/layout/Header";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import Projects from "@/pages/Projects";
+import Contact from "@/pages/Contact";
 
 function App() {
   return (
     <div className="flex justify-center">
-    <div className="flex gap-5 w-[90%] my-8 relative">
+    <div className="flex  flex-col lg:flex-row  gap-5 w-[90%] my-8 relative">
     <Router>
         <Sidebar />
-        <div className="w-full p-8  overflow-y-auto flex-grow-0 bg-primary-bg relative border border-[#383838] rounded-[20px] text-white">
+        <section className="w-full p-8  overflow-y-auto flex-grow-0 bg-primary-bg relative border border-[#383838] rounded-[20px] text-white">
           <Header />
-          <div>
+          <main>
             <Routes>
               <Route path="/" element={<About />} />
               <Route path="/resume" element={<Resume />} />
               <Route path="/projects" element={<Projects />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
-          </div>
-        </div>
+          </main>
+        </section>
       </Router>
     </div>
     </div>
--- src/shared/layout/Sidebar/index.tsx
+++ src/shared/layout/Sidebar/index.tsx
@@ -4,24 +4,31 @@ import {
   MdOutlineLocalPhone,
   MdOutlineLocationOn,
   MdOutlineMailOutline,
+  MdKeyboardArrowDown,
 } from "react-icons/md";
 
 export default function Sidebar() {
   return (
-    <div className="flex flex-col gap-5 items-center h-fit sticky top-8 py-6 px-5 bg-primary-bg border border-[#383838] rounded-[20px] text-white w-[17.5rem]">
-      <div className="flex flex-col  items-center gap-2">
-        <div className="rounded-3xl bg-[#343434] w-36">
-          <div className="w-36 h-36"></div>
+    <div className="flex flex-col gap-5 static items-start lg:items-center h-fit  lg:sticky top-8 py-6 px-5 bg-primary-bg border border-[#383838] rounded-[20px] text-white lg:w-[17.5rem] w-full">
+      <div className="flex flex-row lg:flex-col items-center gap-4 ">
+        <div className="rounded-3xl bg-[#343434] lg:w-36 w-28">
+          <div className="lg:w-36 lg:h-36 w-28 h-28"></div>
         </div>
-        <h1 className="text-head-color text-2xl text-center">
-          Nazrin Isgandarova
-        </h1>
-        <p className="bg-[#2b2b2c] text-xs text-[#ffff] py-2 px-4 rounded-lg">
-          Frontend Developer
-        </p>
+        <div className="flex flex-col gap-2 items-start lg:items-center">
+          <h1 className="text-head-color text-2xl lg:text-center">
+            Nazrin Isgandarova
+          </h1>
+          <p className="bg-[#2b2b2c] text-xs text-[#ffff] py-2 px-4 rounded-lg">
+            Frontend Developer
+          </p>
+        </div>
+        <button className="lg:hidden flex gap-1 items-center bg-[#282829] border border-[#383838] absolute top-0 right-0 rounded-tr-xl rounded-bl-xl py-2 px-3">
+          <MdKeyboardArrowDown className="text-xl text-primary-color max-w-[400px]:block hidden" />
+          <span className="text-sm text-primary-color">Show Contacts</span>
+        </button>
       </div>
-      <div className="w-full h-[1px] bg-head-color bg-[#383838]"></div>
-      <div className="flex flex-col gap-6">
+      <div className="w-full h-[1px] bg-head-color hidden lg:block bg-[#383838]"></div>
+      <div className="flex-col gap-6 hidden lg:flex">
         <div className="flex gap-3 items-center">
           <div className="w-12 h-12 bg-[#202022] bg-custom-gradient shadow-custom rounded-xl flex justify-center items-center">
             <MdOutlineMailOutline className="text-primary-color text-lg" />
@@ -50,7 +57,7 @@ export default function Sidebar() {
           </div>
         </div>
       </div>
-      <div className="flex gap-2">
+      <div className="gap-2 hidden lg:flex">
         <a
           href="https://www.linkedin.com/in/nazrin-isgandarova-4104841a6/"
           target="_blank"
